107ab5446d12bacaea81fa7f10fe9503548b070e
Ethan Metz <perfectverdict@protonmail.com>
2025-04-03 00:29:36 -0400

    FIXED (i hope(

--- src/App.jsx
+++ src/App.jsx
@@ -2,71 +2,22 @@ import "./App.css";
 import Axios from "axios";
 import { useState } from "react";
 function App() {
-<<<<<<< HEAD
-    const [artist, setartist] = useState("");
-    const [song, setsong] = useState("");
-    const [lyrics, setlyrics] = useState("");
-
-    function searchlyrics() {
-        if (artist === "" || song === "") {
-            return;
-        }
-        axios.get(
-            `https://api.lyrics.ovh/v1/${artist}/${song}`).then(res => {
-            console.log(res.data.lyrics);
-            setLyrics(res.data.lyrics);
-        })
-=======
   const [artist, setArtist] = useState("");
   const [song, setSong] = useState("");
   const [lyrics, setLyrics] = useState("");
-
-  function searchLyrics() {
+    function searchLyrics() {
     if (artist === "" || song === "") {
       return;
->>>>>>> fcc4bb8e57ef760eea0988e6ebd6fc10fab8c207
-    }
-    Axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`).then((res) => {
+    } Axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`).then((res) => {
       console.log(res.data.lyrics);
       setLyrics(res.data.lyrics);
     });
   }
 
-<<<<<<< HEAD
-    return (
-        <>
-            <div className="container flex min-h-screen min-w-screen">
-                <div className="flex w-full flex-col text-center justify-center border border-green-400">
-
-                    <h1 className="text-white mb-5 mt-5">Lyrics Finder</h1>
-                    {/* responiveness */}
-                    <div className="flex flex-col sm:flex-row justify-center mb-4 sm:space-x-4">
-                        <input className="p-2 m-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" type="text"
-                            placeholder='Artist name'
-                            onChange={(e) => { setArtist(e.target.value) }} />
-                        <input className="p-2 bp-2 m-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" type="text"
-                            placeholder='Song name'
-                            onChange={(e) => { setSong(e.target.value) }} />
-                    </div>
-                    <div className="flex justify-center pb-5">
-                        <button className="w-32 bg-transparent hover:bg-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent pb-5 rounded"
-                            onClick={() => searchLyrics()}>
-                            Search</button>
-                    </div>
-                    {/* responiveness */}
-
-                    <div className="flex justify-center w-full">
-                        <pre className="lyrics w-full">{lyrics}</pre>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-=======
   return (
     <>
       <div className="container flex min-h-screen min-w-screen">
-        <div className="flex w-full flex-col text-center justify-center border border-green-400">
+          <div className="flex w-full flex-col text-center justify-center border border-green-400">
           <h1 className="text-white mb-5 mt-5">Lyrics Finder</h1>
           {/* responiveness */}
           <div className="flex flex-col sm:flex-row justify-center mb-4 sm:space-x-4">
@@ -80,8 +31,7 @@ function App() {
             />
             <input
               className="p-2 bp-2 m-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              type="text"
-              placeholder="Song name"
+              type="text"              placeholder="Song name"
               onChange={(e) => {
                 setSong(e.target.value);
               }}
@@ -90,13 +40,10 @@ function App() {
           <div className="flex justify-center pb-5">
             <button
               className="w-32 bg-transparent hover:bg-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent pb-5 rounded"
-              onClick={() => searchLyrics()}
-            >
-              Search
+              onClick={() => searchLyrics()}>Search
             </button>
           </div>
           {/* responiveness */}
-
           <div className="flex justify-center w-full">
             <pre className="lyrics w-full">{lyrics}</pre>
           </div>
@@ -104,7 +51,6 @@ function App() {
       </div>
     </>
   );
->>>>>>> fcc4bb8e57ef760eea0988e6ebd6fc10fab8c207
 }
 
-export default App;
+export default App
